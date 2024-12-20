1f3e010292c6daefdb84bb353889a6f856e40643
rizalzaul28 <rizandi778@gmail.com>
2024-12-20 20:40:10 +0700

    Membuat Project Revisi

---
+++ src/newpackage/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package newpackage;
+
+/**
+ *
+ * @author rizan
+ */
+public class NewClass {
+
+}
